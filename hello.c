bfef859cd5128139da5fce27c705bbabdb3da448
rmacdonald00 <42583647+rmacdonald00@users.noreply.github.com>
2018-08-23 14:55:17 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Taylor Kellogg Computer Science\n");
+  printf("Taylor Kellogg Computer Science and Robyn MacDonald Mathematics\n");
 
   return 0;
 }
